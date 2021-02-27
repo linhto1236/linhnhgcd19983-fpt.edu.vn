88540279cade135de9c360acec4b7083239fede1
linhto1236 <66708170+linhto1236@users.noreply.github.com>
2021-02-27 21:11:09 +0700

    Update boss.php

--- boss.php
+++ boss.php
@@ -50,7 +50,7 @@ $result = pg_query($con, $query);
                 <td><?php echo $row['product_id']; ?></td>
                 <td><?php echo $row['product_name']; ?></td>
                 <td><?php echo $row['product_amount']; ?></td>
-                <td><?php echo $row['product_price,']; ?></td>
+                <td><?php echo $row['product_price']; ?></td>
               </tr>
           <?php
             }
