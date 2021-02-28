e4b1040a9896125033403670f620096c3fa5b05e
linhto1236 <66708170+linhto1236@users.noreply.github.com>
2021-02-28 19:44:30 +0700

    Update login.php

--- login.php
+++ login.php
@@ -2,9 +2,9 @@
 session_start();
 $username = $_POST['Username'];
 $password = $_POST['Password'];
-    if ($username == 'admin' && $password == '123') {
+    if ($username == 'staff' && $password == 'staff1236') {
         header("location:staff.html");
-    } elseif ($username == 'boss' && $password == '123') {
+    } elseif ($username == 'boss' && $password == 'boss1236') {
         header("location:boss.php");
     } else
     {
